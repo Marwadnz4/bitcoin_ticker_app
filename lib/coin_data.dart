0de6e4aa31b33fa7a9fda064af0576dc08c416de
marwadnz4 <marwa.mohammed.jassim@gmail.com>
2022-11-21 23:42:12 +0300

    first commit

---
+++ lib/coin_data.dart
@@ -0,0 +1,56 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+const List<String> currenciesList = [
+  'AUD',
+  'BRL',
+  'CAD',
+  'CNY',
+  'EUR',
+  'GBP',
+  'HKD',
+  'IDR',
+  'ILS',
+  'INR',
+  'JPY',
+  'MXN',
+  'NOK',
+  'NZD',
+  'PLN',
+  'RON',
+  'RUB',
+  'SEK',
+  'SGD',
+  'USD',
+  'ZAR'
+];
+
+const List<String> cryptoList = [
+  'BTC',
+  'ETH',
+  'LTC',
+];
+
+const coinAPIURL = 'https://rest.coinapi.io/v1/exchangerate';
+const apiKey = '8D5C3D90-3F6D-493D-8FF1-A4C8F213BC62';
+
+class CoinData {
+  Future getCoinData(String selectedCurrency) async {
+    Map<String, String> results = {};
+    for (String crypto in cryptoList) {
+      String requestURL =
+          '$coinAPIURL/$crypto/$selectedCurrency?apikey=$apiKey';
+      http.Response response = await http.get(Uri.parse(requestURL));
+      if (response.statusCode == 200) {
+        var decodedData = jsonDecode(response.body);
+        print(decodedData);
+        double lastPrice = decodedData['rate'];
+        results[crypto] = lastPrice.toStringAsFixed(0);
+      } else {
+        print(response.statusCode);
+        throw 'Problem with the get request';
+      }
+    }
+    return results;
+  }
+}
